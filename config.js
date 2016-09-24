2d73551854a69b3b392006fe798c63512666e7eb
Jared Hanson <jaredhanson@gmail.com>
2016-09-23 18:15:44 -0700

    Add config.js.

---
+++ config.js
@@ -0,0 +1,13 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: false,
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  map: {
+    "querystring": "github:anchorjs/querystring@0.0.1"
+  }
+});
