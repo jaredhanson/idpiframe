18ea425c1c7e4d06a46bebc3ca826b673fadbb89
Jared Hanson <jaredhanson@gmail.com>
2016-09-23 19:49:01 -0700

    Add IFrame class.

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-define(['exports', 'querystring'],
-function(exports, qs) {
+define(['./lib/iframe', 'querystring', 'exports'],
+function(IFrame, qs, exports) {
+  
+  function postEvent(type) {
+    
+  }
+  
+  
+  var iframe;
   
   exports.start = function() {
+    
     var hash = window.location.hash.slice(1); // slice off leading `#`
     var q = qs.parse(hash);
     console.log(q)
@@ -9,27 +17,8 @@ function(exports, qs) {
     var origin = q.origin;
     var rpcToken = q.rpcToken;
     
-    
-    window.addEventListener('message', function (event) {
-      if (event.origin !== origin) { return; }
-      if (event.source !== window.parent) { return; }
-      // TODO: try/catch
-      var msg = JSON.parse(event.data);
-      if (msg.rpcToken !== rpcToken) { return; }
-
-      console.log('msg: ');
-      console.log(msg);
-    
-    });
-  
-    window.addEventListener('storage', function(e) {  
-      console.log('GOT STORAGE EVENT!');
-      console.log(e);
-    });
-    
-    window.parent.postMessage(
-      JSON.stringify({"method":"fireIdpEvent","params":{"type":"idpReady"},"rpcToken":"8462679"}),
-      'http://localhost:3001'); // TODO: get this from hash
+    iframe = new IFrame(q.origin, q.rpcToken);
+    iframe.bind();
   };
   
 });
