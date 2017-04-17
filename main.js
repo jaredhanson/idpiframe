9303c90b3473b4774b277d2f1652bbf4a9db01af
Jared Hanson <jaredhanson@gmail.com>
2017-04-17 14:35:21 -0700

    Pass query to listSessions.

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 define(['./lib/iframe', 'querystring', 'exports'],
 function(IFrame, qs, exports) {
   
-  function postEvent(type) {
-    
-  }
-  
-  
   var iframe;
   
   exports.start = function() {
-    
-    var hash = window.location.hash.slice(1); // slice off leading `#`
+    var hash = window.location.hash.substring(1); // slice off leading `#`
     var q = qs.parse(hash);
     console.log(q)
 
