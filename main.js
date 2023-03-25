b145456c1289af026ac14ce20756983ef13e6b65
Jared Hanson <jaredhanson@gmail.com>
2023-03-25 06:54:00 -0700

    Add TODO item.

--- main.js
+++ main.js
@@ -7,11 +7,14 @@ function(IFrame, qs, exports) {
     var hash = window.location.hash.substring(1); // slice off leading `#`
     var q = qs.parse(hash);
     
+    
+    // TODO: Implement check so that idpReady event is not sent if q.origin isn't valid.
     // FIXME: Google's implemenation still delivers authResult from popup in this
     // condition.   Make that work.
-    if (location.origin !== q.origin) {
-      return;
-    }
+    // TODO: Test what happens if iframe receives a message in this condition (ie, monitorClient)
+    //if (location.origin !== q.origin) {
+    //  return;
+    //}
     
     // TODO: pass the clearCache option
     iframe = new IFrame(q.origin, q.rpcToken);
